febdc28b46b100b8842bc5204a62acf10a741007
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2014-11-20 00:44:52 -0800

    Change to use classes

--- js/game.js
+++ js/game.js
@@ -1,45 +1,49 @@
-// Create the canvas
+// Create canvas
+
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
+var then;
 canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
 
 // Background image
+
 var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function () {
 	bgReady = true;
 };
+
 bgImage.src = "images/background.png";
 
-// Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
-	heroReady = true;
-};
-heroImage.src = "images/hero.png";
+var Hero = function () {
+	this.sprite = "images/hero.png";
+	this.speed = 256;
+	this.x = 0;
+	this.y = 0;
+}
 
-// Monster image
-var monsterReady = false;
-var monsterImage = new Image();
-monsterImage.onload = function () {
-	monsterReady = true;
+Hero.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-monsterImage.src = "images/monster.png";
 
-// Game objects
-var hero = {
-	speed: 256 // movement in pixels per second
+var Monster = function() {
+	this.sprite = "images/monster.png";
+	this.x = 0;
+	this.y = 0;
 };
-var monster = {};
+
+Monster.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 var monstersCaught = 0;
 
 // Handle keyboard controls
 var keysDown = {};
 
-addEventListener("keydown", function (e) {
+addEventListener("keydown", function(e){
 	keysDown[e.keyCode] = true;
 }, false);
 
@@ -48,66 +52,66 @@ addEventListener("keyup", function (e) {
 }, false);
 
 // Reset the game when the player catches a monster
-var reset = function () {
+
+var reset = function() {
 	hero.x = canvas.width / 2;
 	hero.y = canvas.height / 2;
 
-	// Throw the monster somewhere on the screen randomly
+	// Throw monster on screen randomly
 	monster.x = 32 + (Math.random() * (canvas.width - 64));
 	monster.y = 32 + (Math.random() * (canvas.height - 64));
 };
 
 // Update game objects
-var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
+
+var update = function (dt) {
+	if (38 in keysDown) { // Player holding up 
+		hero.y -= hero.speed * dt;
 	}
 	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
+		hero.y += hero.speed * dt;
 	}
 	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
+		hero.x -= hero.speed * dt;
 	}
 	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
+		hero.x += hero.speed * dt;
 	}
+	// Collision?
 
-	// Are they touching?
-	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
+if (
+		hero.x <= (monster.x + 32) &&
+		monster.x <= (hero.x + 32) &&
+		hero.y <= (monster.y + 32) &&
+		monster.y <= (hero.y + 32)
 	) {
 		++monstersCaught;
-		reset();
+	reset();
 	}
 };
 
-// Draw everything
-var render = function () {
-	if (bgReady) {
-		ctx.drawImage(bgImage, 0, 0);
+// Draw all the things
+
+var render = function() {
+	if (bgReady){
+		ctx.drawImage(bgImage,0,0);
 	}
 
-	if (heroReady) {
-		ctx.drawImage(heroImage, hero.x, hero.y);
-	}
-
-	if (monsterReady) {
-		ctx.drawImage(monsterImage, monster.x, monster.y);
-	}
+	hero.render();
+	monster.render();
 
 	// Score
-	ctx.fillStyle = "rgb(250, 250, 250)";
+
+	ctx.fillStyle = "rgb(250,250,250";
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+	ctx.fillText("Monsters caught: " + monstersCaught, 32, 32);
 };
 
-// The main game loop
-var main = function () {
+// Main game loop
+
+var main = function() {
 	var now = Date.now();
 	var delta = now - then;
 
@@ -120,11 +124,22 @@ var main = function () {
 	requestAnimationFrame(main);
 };
 
-// Cross-browser support for requestAnimationFrame
+// Cross browser support for looping
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
-// Let's play this game!
-var then = Date.now();
-reset();
-main();
+// Play!
+var hero = new Hero();
+var monster = new Monster();
+
+function init() {
+  var then = Date.now();
+	reset();
+	main();
+}
+Resources.load([
+	"images/background.png",
+	"images/hero.png",
+	"images/monster.png"
+]);
+Resources.onReady(init);
